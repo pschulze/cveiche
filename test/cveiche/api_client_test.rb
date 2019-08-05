e01201f09966c98c91a8225c69ea112610b8df0d
Peter Schulze <peterkschulze@gmail.com>
2019-08-04 22:47:25 -0600

    create ApiClient test stubs

---
+++ test/cveiche/api_client_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class ApiClientTest < Minitest::Test
+  def test_list_vendors
+
+  end
+
+  def test_products_for_vendor
+
+  end
+
+  def test_products_for_vendor_reutrns_nil_for_invalid_vendor
+
+  end
+
+  def test_cves_for_vendor_product
+
+  end
+
+  def test_cves_for_vendor_product_returns_nil_for_invalid_product
+
+  end
+
+  def test_cves_for_vendor_product_returns_nil_for_invalid_vendor
+
+  end
+
+  def test_cve_for_id
+
+  end
+
+  def test_cve_for_id_returns_nil_for_invalid_cve_id
+
+  end
+end
