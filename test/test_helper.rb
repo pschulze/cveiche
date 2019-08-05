d0d14432cd3d353b526236a737ead0028ee4f61c
Peter Schulze <peterkschulze@gmail.com>
2019-08-05 11:07:39 -0600

    Add webmock and vcr config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,12 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "cveiche"
+# frozen_string_literal: true
 
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path('../../lib', __dir__)
+require 'cveiche'
+require 'minitest/autorun'
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/fixtures'
+  config.hook_into :webmock
+end
